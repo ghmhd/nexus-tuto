9fbe5ce628118291a375c591938d3c701c149ed6
Gharbi Mohamed <ghmhdweb1996@gmail.com>
2020-08-09 00:08:36 +0100

    add drafts [Query]

--- api/graphql/Post.ts
+++ api/graphql/Post.ts
@@ -9,3 +9,24 @@ schema.objectType({
     t.boolean("published");
   },
 });
+
+schema.extendType({
+  type: "Query",
+  definition(t) {
+    t.field("drafts", {
+      nullable: false,
+      type: "Post",
+      list: true,
+      resolve() {
+        return [
+          {
+            id: 1,
+            title: "tutorial",
+            body: "first query",
+            published: false,
+          },
+        ];
+      },
+    });
+  },
+});
