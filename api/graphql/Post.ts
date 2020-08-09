0a6f4a664707d9d016082d9b66de1ea61fe5a2db
Gharbi Mohamed <ghmhdweb1996@gmail.com>
2020-08-09 01:33:31 +0100

    add createDraft publish [Mutation.Post] | add posts [Query.Post]

--- api/graphql/Post.ts
+++ api/graphql/Post.ts
@@ -17,15 +17,52 @@ schema.extendType({
       nullable: false,
       type: "Post",
       list: true,
-      resolve() {
-        return [
-          {
-            id: 1,
-            title: "tutorial",
-            body: "first query",
-            published: false,
-          },
-        ];
+      resolve(_root, _args, ctx, _info) {
+        return ctx.db.posts.filter((post) => post.published === false);
+      },
+    });
+    t.list.field("posts", {
+      type: "Post",
+      nullable: false,
+      resolve(_root, _args, ctx) {
+        return ctx.db.posts.filter((post) => post.published === true);
+      },
+    });
+  },
+});
+
+schema.extendType({
+  type: "Mutation",
+  definition(t) {
+    t.field("createDraft", {
+      type: "Post",
+      args: {
+        body: schema.stringArg({ required: true }),
+        title: schema.stringArg({ required: true }),
+      },
+      resolve(_root, args, ctx) {
+        let draft = {
+          id: ctx.db.posts.length + 1,
+          ...args,
+          published: false,
+        };
+        ctx.db.posts.push(draft);
+        return draft;
+      },
+    });
+
+    t.field("publish", {
+      type: "Post",
+      args: {
+        id: schema.intArg({ required: true }),
+      },
+      resolve(_root, args, ctx) {
+        let draft = ctx.db.posts.find((post) => post.id === args.id);
+        if (!draft) {
+          throw `the draft with id ${args.id}, does not exist`;
+        }
+        draft.published = true;
+        return draft;
       },
     });
   },
